88610bb969211d5096db2def9480104ae3a51257
akirasonypicture34 <akirasonypicture34@gmail.com>
2017-07-26 14:45:14 +0200

    textsearch_initdata

--- Taxi/TaxiSearchResultViewController.m
+++ Taxi/TaxiSearchResultViewController.m
@@ -14,6 +14,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self initData];
+    [self initView];
+}
+
+- (void)initData{
+
+}
+-(void)initView{
+    NSString *initialText = @"example";
 }
 
 - (void)didReceiveMemoryWarning {
